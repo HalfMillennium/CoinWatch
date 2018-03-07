0ceddfb35c6ebd1449a806527f62c3a9862c66d0
gchestnut_Anduril <glc2266@gmail.com>
2018-03-07 10:15:27 -0500

    v1.0
    Fetches articles, and lists them with a title, source, description, and date published.

--- app/src/main/java/com/cryptonow/anduril/cryptonow/MainActivity.java
+++ app/src/main/java/com/cryptonow/anduril/cryptonow/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,22 +22,59 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+    private SimpleAdapter sa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new JsonTask().execute("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7b9a5818d54749d4b91fcb7639bd3278");
+
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        /*
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        */
+
+        new JsonTask().execute("https://newsapi.org/v2/everything?q=bitcoin AND (cryptocurrency OR ethereum)&language=en&sortBy=publishedAt&pageSize=100&apiKey=7b9a5818d54749d4b91fcb7639bd3278");
+    }
+
+    private Date today() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 0);
+        return cal.getTime();
+    }
+
+    private String getTodaysDateString() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        return dateFormat.format(today());
     }
 
     private class JsonTask extends AsyncTask<String, String, String> {
 
         String txtJson = null;
         ArrayList<JSONObject> art = new ArrayList<>();
+        HashMap<String,String> item;    //Used to link data to lines
+
+        String[] titles, desc, names, dates;
 
         protected void onPreExecute() {
             super.onPreExecute();
@@ -60,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line+"\n");
-                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+                    Log.d("Response: ", "> " + line);   // full response
 
                 }
 
@@ -92,32 +133,95 @@ public class MainActivity extends AppCompatActivity {
 
             txtJson = result;
 
+            Log.d("test result", txtJson);
+
             try {
-                JSONObject full = new JSONObject(txtJson);
-                writeToFile(txtJson, MainActivity.this);
-                JSONArray articles = new JSONArray(full.get("articles"));
-                Log.d("TITLE", "test");
+                JSONObject txt = new JSONObject(txtJson);
+                JSONArray articles = txt.getJSONArray("articles");
+
+                Log.d("test-tag", "" + articles.length());
+
+
+                buildList(articles);
+
 
                 for(int i = 0; i < articles.length(); i++)
                 {
                     JSONObject obj = articles.getJSONObject(i);
-                    Log.d("TITLE", obj.get("title").toString());
+                    Log.d("thethirdroom", obj.toString());
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
 
-        public void writeToFile(String data, Context context) {
+        public String formatDate(String raw)
+        {
+            String inputPattern = "yyyy-MM-dd";
+            String outputPattern = "dd-MMM-yyyy";
+            SimpleDateFormat inputFormat = new SimpleDateFormat(inputPattern);
+            SimpleDateFormat outputFormat = new SimpleDateFormat(outputPattern);
+
+            Date date = null;
+            String str = null;
+
             try {
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("articles.json", Context.MODE_PRIVATE));
-                outputStreamWriter.write(data);
-                outputStreamWriter.close();
+                date = inputFormat.parse(raw);
+                str = outputFormat.format(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
-            catch (IOException e) {
-                Log.e("Exception", "File write failed: " + e.toString());
-            }
+            return str;
         }
+
+
+        public void buildList(JSONArray arr)
+        {
+            titles = new String[arr.length()];
+            desc = new String[arr.length()];
+            names = new String[arr.length()];
+            dates = new String[arr.length()];
+
+
+            for(int i = 0; i < arr.length(); i++)
+            {
+                JSONObject article = null;
+
+                try {
+                    article = arr.getJSONObject(i);
+
+                    titles[i] = article.get("title").toString();
+                    desc[i] = article.get("description").toString();
+
+                    JSONObject source = article.getJSONObject("source");
+                    names[i] = source.get("name").toString();
+
+                    dates[i] = formatDate(article.get("publishedAt").toString().substring(0, 10));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Load the data
+            for(int i = 0; i < arr.length(); i++){
+                item = new HashMap<String,String>();
+                item.put("line1", titles[i]);
+                item.put("line2", names[i]);
+                item.put("line3", desc[i]);
+                item.put("line4", dates[i]);
+                list.add(item);
+            }
+
+            //Use an Adapter to link data to Views
+            sa = new SimpleAdapter(MainActivity.this, list,
+                    R.layout.fourlines,
+                    new String[] { "line1","line2", "line3", "line4"},
+                    new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d});
+            //Link the Adapter to the list
+            ((ListView)findViewById(R.id.articleView)).setAdapter(sa);
+        }
+
+
     }
 }
 
